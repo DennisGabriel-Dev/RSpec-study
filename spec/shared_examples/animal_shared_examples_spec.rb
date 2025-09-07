61d04d518f1378c972c2e9d15a64fffb7381edbe
DennisGabriel <dennisgabr745@gmail.com>
2025-09-06 23:59:14 -0300

    more shared examples

--- spec/shared_examples/animal_shared_examples_spec.rb
+++ spec/shared_examples/animal_shared_examples_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../../lib/shared_examples/dog'
 require_relative '../../lib/shared_examples/domestic_animal'
+require_relative '../../lib/shared_examples/cat'
+require_relative '../../lib/shared_examples/lion'
+
 
 RSpec.shared_examples "an animal" do
   it "has a name" do
@@ -46,5 +49,37 @@ RSpec.describe Dog do
     it "can fetch a ball" do
       expect(subject.fetch("ball")).to eq("Rex fetched the ball!")
     end
+  end
+end
+
+RSpec.describe Cat do
+  subject { Cat.new("Whiskers") }
+
+  it_behaves_like "a domestic animal"
+
+  describe "#make_sound" do
+    it "meows" do
+      expect(subject.make_sound).to eq("Meow!")
+    end
+  end
+
+  describe "#hunt" do
+    it "can hunt a mouse" do
+      expect(subject.hunt("mouse")).to eq("Whiskers caugh a mouse!")
+    end
+  end
+end
+
+RSpec.describe Lion do
+  subject { Lion.new("Simba") }
+
+  it_behaves_like "an animal"
+
+  it "#make_sound" do
+    expect(subject.make_sound).to eq("ROAR!")
+  end
+
+  it "#hunt" do
+    expect(subject.hunt("fox")).to eq("Simba is hunting fox with the pack!")
   end
 end
